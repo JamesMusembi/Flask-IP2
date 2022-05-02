8fd5eb96282ba1addd21d125f0f5e73ed92a71aa
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-02 17:55:05 +0300

    fixing views

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-# MOVIE_API_KEY='1b0f2bd179f44636bef9b3fe9da611dd'
+MOVIE_API_KEY='1b0f2bd179f44636bef9b3fe9da611dd'
 
-# SECRET_KEY='mysecretkey'
+SECRET_KEY='mysecretkey'
 
-# python3.9 manager.py server
+python3.9 manager.py server
