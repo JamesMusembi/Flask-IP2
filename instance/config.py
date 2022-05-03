f98454026901b28428c1fe4370da0567a1a50e8d
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-04 00:52:01 +0300

    setting up key

--- instance/config.py
+++ instance/config.py
@@ -2,4 +2,4 @@ API_KEY='52205e10a9fc49e797ad3a9088babfea'
 # BASE_URL='https://newsapi.org/v2/everything'
 SOURCES_URL='https://newsapi.org/v2/top-headlines/sources'
 
-SECRET_KEY='mysecretkey'
+SECRET_KEY='akIolfXFO231tUnRmDi1v1oHIi61Hlbq'
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 MOVIE_API_KEY='52205e10a9fc49e797ad3a9088babfea'
 
-SECRET_KEY='mysecretkey'
+SECRET_KEY='akIolfXFO231tUnRmDi1v1oHIi61Hlbq'
 
 python3.9 manage.py server
