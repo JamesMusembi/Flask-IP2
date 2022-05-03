f13bbd1b1b4e9b9842650fb19aafa23660d01e8c
JamesMusembi <james.musembi@student.moringaschool.com>
2022-05-04 00:17:43 +0300

    cli install

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
 API_KEY='52205e10a9fc49e797ad3a9088babfea'
-BASE_URL='https://newsapi.org/v2/everything'
+# BASE_URL='https://newsapi.org/v2/everything'
 SOURCES_URL='https://newsapi.org/v2/top-headlines/sources'
 
 SECRET_KEY='mysecretkey'
